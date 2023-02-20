157d2a440fdacf5f91066cef374c64e957de493b
Natália Fonseca <natalia@nataliafonseca.dev>
2023-02-20 15:31:43 -0300

    fix: fix android plugin

--- zsh/plugins/android/android.plugin.zsh
+++ zsh/plugins/android/android.plugin.zsh
@@ -2,8 +2,6 @@ is_mac &&
 export ANDROID_HOME=~/Library/Android/Sdk || 
 export ANDROID_HOME=~/.config/Android/Sdk
 
-export JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64
-export ANDROID_HOME=$HOME/.local/share/Android/Sdk
 export PATH=$PATH:$ANDROID_HOME/emulator
 export PATH=$PATH:$ANDROID_HOME/tools
 export PATH=$PATH:$ANDROID_HOME/tools/bin
