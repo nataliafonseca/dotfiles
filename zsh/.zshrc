480c5881aa4e1dff4ce0ef89c5c85dcbc858a4bc
Natália Fonseca <natalia@nataliafonseca.dev>
2024-02-02 22:25:24 -0300

    wip

--- zsh/.zshrc
+++ zsh/.zshrc
@@ -42,10 +42,3 @@ source $zsh_plugins
 
 # load aliases
 source $ZDOTDIR/.zaliases
-
-####
-function gifconvert() {
-    ffmpeg -y -i "$1" -vf palettegen _tmp_palette.png
-    ffmpeg -y -i "$1" -i _tmp_palette.png -filter_complex paletteuse -r 10  "${1%.webm}.gif"
-    rm _tmp_palette.png
-}
---
+++ zsh/plugins/subsearch/subsearch.plugin.zsh
@@ -0,0 +1,7 @@
+bindkey "$terminfo[kcuu1]" history-substring-search-up
+bindkey "$terminfo[kcud1]" history-substring-search-down
+bindkey '^[[A' history-substring-search-up
+bindkey '^[OA' history-substring-search-up
+bindkey '^[[B' history-substring-search-down
+bindkey '^[OB' history-substring-search-down
+HISTORY_SUBSTRING_SEARCH_ENSURE_UNIQUE=1
