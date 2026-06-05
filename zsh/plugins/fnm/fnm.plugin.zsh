e4e8d4854fcd90a615c71f574643f329031664f1
Natália Fonseca <natalia@nataliafonseca.dev>
2026-06-05 11:39:23 -0300

    fix: fix fnm plugin

--- zsh/plugins/fnm/fnm.plugin.zsh
+++ zsh/plugins/fnm/fnm.plugin.zsh
@@ -1,4 +1,12 @@
-FNM_PATH=$(which fnm)
+if [ -d "$XDG_DATA_HOME/fnm" ]; then
+  FNM_PATH="$XDG_DATA_HOME/fnm"
+elif [ -d "/opt/homebrew/opt/fnm/bin" ]; then
+  FNM_PATH="/opt/homebrew/opt/fnm/bin"
+fi
+
 if [ -d "$FNM_PATH" ]; then
+  if (( ! $+commands[fnm] )); then
+    export PATH="$PATH:$FNM_PATH"
+  fi
   eval "$(fnm env --shell zsh)"
 fi
